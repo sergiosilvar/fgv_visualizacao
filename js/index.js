92c550d2c5d1935e9453915f8e8797bd2fbcd59c
Sérgio Rodrigues <srodriguex@gmail.com>
2014-05-08 16:10:19 -0300

    Gantt funcionando oline.

--- js/index.js
+++ js/index.js
@@ -148,6 +148,7 @@ function converte_nvd3(cen) {
 	return series
 }
 
+/*
 function grafico_gantt(cen){	
 	var  qtd_tanques = cen.nome_tanques.length;
 	var height = 300,
@@ -166,11 +167,11 @@ function grafico_gantt(cen){
         .domain([0, cen.num_unid_tempo])
         .range([0,x_axis_length])
         ;
-/*
-    var y = d3.scale.linear()
-        .domain([cen.qtd_tanques+1, 1])
-        .range([0, y_axis_length]);
-*/
+
+//    var y = d3.scale.linear()
+//        .domain([cen.qtd_tanques+1, 1])
+//        .range([0, y_axis_length]);
+
     var y = d3.scale.ordinal()
         .domain([y_axis_length,0])
         .range([0,y_axis_length]);
@@ -258,48 +259,15 @@ function grafico_gantt(cen){
             .attr("x2", x_axis_length) // <-F
             .attr("y2", 0);
 } 
-
+*/
 
 function gantt_d3(cen){
-/*
-	 tasks = [
-	{"startDate":new Date("Sun Dec 09 01:36:45 EST 2012"),"endDate":new Date("Sun Dec 09 02:36:45 EST 2012"),"taskName":"E Job","status":"RUNNING"},
-	{"startDate":new Date("Sun Dec 09 04:56:32 EST 2012"),"endDate":new Date("Sun Dec 09 06:35:47 EST 2012"),"taskName":"A Job","status":"RUNNING"},
-	{"startDate":new Date("Sun Dec 09 06:29:53 EST 2012"),"endDate":new Date("Sun Dec 09 06:34:04 EST 2012"),"taskName":"D Job","status":"RUNNING"},
-	{"startDate":new Date("Sun Dec 09 05:35:21 EST 2012"),"endDate":new Date("Sun Dec 09 06:21:22 EST 2012"),"taskName":"P Job","status":"RUNNING"},
-	{"startDate":new Date("Sun Dec 09 05:00:06 EST 2012"),"endDate":new Date("Sun Dec 09 05:05:07 EST 2012"),"taskName":"D Job","status":"RUNNING"},
-	{"startDate":new Date("Sun Dec 09 03:46:59 EST 2012"),"endDate":new Date("Sun Dec 09 04:54:19 EST 2012"),"taskName":"P Job","status":"RUNNING"},
-	{"startDate":new Date("Sun Dec 09 04:02:45 EST 2012"),"endDate":new Date("Sun Dec 09 04:48:56 EST 2012"),"taskName":"N Job","status":"RUNNING"},
-	{"startDate":new Date("Sun Dec 09 03:27:35 EST 2012"),"endDate":new Date("Sun Dec 09 03:58:43 EST 2012"),"taskName":"E Job","status":"SUCCEEDED"},
-	{"startDate":new Date("Sun Dec 09 01:40:11 EST 2012"),"endDate":new Date("Sun Dec 09 03:26:35 EST 2012"),"taskName":"A Job","status":"SUCCEEDED"},
-	{"startDate":new Date("Sun Dec 09 03:00:03 EST 2012"),"endDate":new Date("Sun Dec 09 03:09:51 EST 2012"),"taskName":"D Job","status":"SUCCEEDED"},
-	{"startDate":new Date("Sun Dec 09 01:21:00 EST 2012"),"endDate":new Date("Sun Dec 09 02:51:42 EST 2012"),"taskName":"P Job","status":"SUCCEEDED"},
-	{"startDate":new Date("Sun Dec 09 01:08:42 EST 2012"),"endDate":new Date("Sun Dec 09 01:33:42 EST 2012"),"taskName":"N Job","status":"FAILED"},
-	{"startDate":new Date("Sun Dec 09 00:27:15 EST 2012"),"endDate":new Date("Sun Dec 09 00:54:56 EST 2012"),"taskName":"E Job","status":"SUCCEEDED"},
-	{"startDate":new Date("Sun Dec 09 00:29:48 EST 2012"),"endDate":new Date("Sun Dec 09 00:44:50 EST 2012"),"taskName":"D Job","status":"SUCCEEDED"},
-	{"startDate":new Date("Sun Dec 09 07:39:21 EST 2012"),"endDate":new Date("Sun Dec 09 07:43:22 EST 2012"),"taskName":"P Job","status":"RUNNING"},
-	{"startDate":new Date("Sun Dec 09 07:00:06 EST 2012"),"endDate":new Date("Sun Dec 09 07:05:07 EST 2012"),"taskName":"D Job","status":"RUNNING"},
-	{"startDate":new Date("Sun Dec 09 08:46:59 EST 2012"),"endDate":new Date("Sun Dec 09 09:54:19 EST 2012"),"taskName":"P Job","status":"RUNNING"},
-	{"startDate":new Date("Sun Dec 09 09:02:45 EST 2012"),"endDate":new Date("Sun Dec 09 09:48:56 EST 2012"),"taskName":"N Job","status":"RUNNING"},
-	{"startDate":new Date("Sun Dec 09 08:27:35 EST 2012"),"endDate":new Date("Sun Dec 09 08:58:43 EST 2012"),"taskName":"E Job","status":"SUCCEEDED"},
-	{"startDate":new Date("Sun Dec 09 08:40:11 EST 2012"),"endDate":new Date("Sun Dec 09 08:46:35 EST 2012"),"taskName":"A Job","status":"SUCCEEDED"},
-	{"startDate":new Date("Sun Dec 09 08:00:03 EST 2012"),"endDate":new Date("Sun Dec 09 08:09:51 EST 2012"),"taskName":"D Job","status":"SUCCEEDED"},
-	{"startDate":new Date("Sun Dec 09 10:21:00 EST 2012"),"endDate":new Date("Sun Dec 09 10:51:42 EST 2012"),"taskName":"P Job","status":"SUCCEEDED"},
-	{"startDate":new Date("Sun Dec 09 11:08:42 EST 2012"),"endDate":new Date("Sun Dec 09 11:33:42 EST 2012"),"taskName":"N Job","status":"FAILED"},
-	{"startDate":new Date("Sun Dec 09 12:27:15 EST 2012"),"endDate":new Date("Sun Dec 09 12:54:56 EST 2012"),"taskName":"E Job","status":"SUCCEEDED"},
-	{"startDate":new Date("Sat Dec 08 23:12:24 EST 2012"),"endDate":new Date("Sun Dec 09 00:26:13 EST 2012"),"taskName":"A Job","status":"KILLED"}];
-*/
-     transform_date = function(d,tam){
-        return new Date(1900,1,d+tam);
-    }
     
     tasks = []
     for (k in cen.situacao){
         for(var i in d3.range(cen.situacao[k].length)){
             sit = cen.situacao[k][i];
             tasks.push({
-                'startDate':transform_date(sit.inicio,0), 
-                'endDate': transform_date(sit.fim,sit.tam), 
                 'taskName':k, 
                 'status':sit.estado,
                 'inicio': sit.inicio,
@@ -319,7 +287,7 @@ function gantt_d3(cen){
 
 	//var taskNames = [ "D Job", "P Job", "E Job", "A Job", "N Job" ];
     taskNames = cen.nome_tanques;
-
+/*
 	tasks.sort(function(a, b) {
 		return a.endDate - b.endDate;
 	});
@@ -330,8 +298,8 @@ function gantt_d3(cen){
 	var minDate = tasks[0].startDate;
 
 	var format = "%H:%M";
-
-	var gantt = d3.gantt().taskTypes(taskNames).taskStatus(taskStatus).tickFormat(format);
+*/
+	var gantt = d3.gantt().taskTypes(taskNames).taskStatus(taskStatus);//.tickFormat(format);
 	gantt(tasks);
 
 }
